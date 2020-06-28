07a94a1f85fd7d6dd1f4f8603a2375a25f131145
min <48940875+Evelyn0116@users.noreply.github.com>
2020-06-28 15:06:15 -0700

    post create update delete

--- instaDemo/insta/urls.py
+++ instaDemo/insta/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from insta.views import HelloWorld
+from insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloWord'),
+    path('', HelloWorld.as_view(), name='helloWorld'),
+    path('posts/', PostView.as_view(), name='posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name ='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name ='post_delete'),
 ]
--- instaDemo/insta/views.py
+++ instaDemo/insta/views.py
@@ -1,6 +1,34 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from insta.models import Post
 
 # 让自定义helloWorld view 继承了template view，就可以用templateView里的很多func和attribute
+#view 里即需要templeta也需要model
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields =['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
